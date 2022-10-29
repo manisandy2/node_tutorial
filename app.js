a823e4b25731167a0635ddbbbfea0d82d222acf1
unknown <mani.sandy2@gmail.com>
2022-10-29 11:59:21 +0530

    node tutorial first intro.js file upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const number = 19
+
+if(number <= 20){
+    console.log("sorry you'r age too young")
+}else{
+    console.log("done")
+}
+
+console.log("first project")
---
+++ intro.js
@@ -0,0 +1,9 @@
+const number = 21
+
+if(number <= 20){
+    console.log("sorry you'r age too young")
+}else{
+    console.log("You,r age ",number )
+}
+
+console.log("first project")
