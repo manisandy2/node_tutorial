447a9515bf9f87fdb86216dd33d7ba03289c0e9c
unknown <mani.sandy2@gmail.com>
2022-10-29 13:02:55 +0530

    1-7 update

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const name = require("./4-name")
+const sayHi = require("./5-utils")
+const data = require("./6-alternative-flover")
+
+require("./7-mind")
+// console.log(data)
+// sayHi(name.durai)
+// sayHi(name.mani)
+// sayHi(name.kavi)
+// sayHi("sfs")
